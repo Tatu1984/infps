669514dd6f68883bdb5b367e6a6c1ff4847fd43b
Dragoon4002 <sounak.dey2468@gmail.com>
2026-06-09 15:34:50 +0530

    Ui(Partnership):Proxmox and Intel Logo Update

--- src/components/sections/HeroSectionNew.tsx
+++ src/components/sections/HeroSectionNew.tsx
@@ -26,10 +26,12 @@ export const HeroSectionNew = () => (
           <BlurText text={heroContent.subheadline} delay={1} />
         </p>
 
-        {/* Proxmox Partnership */}
+        {/* Partnership */}
         <div className="proxmox-badge">
           <span>AUTHORIZED</span>
-          <img src="/Proxmox_logos_full_lockup_SVG/proxmox-full-lockup-color.svg" alt="Proxmox" className="proxmox-logo" loading="lazy" decoding="async" />
+          <img src="/Proxmox_logos_full_lockup_SVG/proxmox-full-lockup-inverted-color.svg" alt="Proxmox" className="proxmox-logo" loading="lazy" decoding="async" />
+          <span>&</span>
+          <img src="/ip-badge-hotizontal-digital/ip-badge-horizontal-3000.png" alt="Infiniti Tech Partners" className="proxmox-logo ip-logo" loading="lazy" decoding="async" />
           <span>RESELLER</span>
         </div>
 
