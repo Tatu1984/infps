aa7eb80bbef9e37394e32b103556d7c328bca312
Dragoon4002 <sounak.dey2468@gmail.com>
2026-06-10 14:20:32 +0530

    UI(logo): Alignment fixed in column

--- src/components/sections/HeroSectionNew.tsx
+++ src/components/sections/HeroSectionNew.tsx
@@ -29,10 +29,12 @@ export const HeroSectionNew = () => (
         {/* Partnership */}
         <div className="proxmox-badge">
           <span>AUTHORIZED</span>
-          <img src="/Proxmox_logos_full_lockup_SVG/proxmox-full-lockup-inverted-color.svg" alt="Proxmox" className="proxmox-logo" loading="lazy" decoding="async" />
-          <span>RESELLER</span>
-          <span>&</span>
           <img src="/ip-badge-hotizontal-digital/ip-badge-horizontal-3000.png" alt="Infiniti Tech Partners" className="proxmox-logo ip-logo" loading="lazy" decoding="async" />
+          <span>&</span>
+          <span className="proxmox-group">
+            <img src="/Proxmox_logos_full_lockup_SVG/proxmox-full-lockup-inverted-color.svg" alt="Proxmox" className="proxmox-logo" loading="lazy" decoding="async" />
+            <span>RESELLER</span>
+          </span>
         </div>
 
         {/* CTAs - Primary leads to consultation, Secondary to social proof */}
